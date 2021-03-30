4b4d94245fb58513689c51ec81e29c3e90a7363a
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2021-03-30 16:20:53 -0500

    Commit on 3/30/21

---
+++ FootballChatbot.py
@@ -0,0 +1,16 @@
+# Kirtana Kalavagunta
+# Ananth Salagundi 
+# CS 4395.0W1
+# Due: 3/4/2021
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import pickle
+import random
+import string
+
+
+if __name__ == '__main__':
+    # Main code here
