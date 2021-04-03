efa7e8152791e2747be6de7de78ac610e94288ee
ajsalagundi <33690110+ajsalagundi@users.noreply.github.com>
2021-04-03 17:40:23 -0500

    finally finished the kb

--- BuildFootballKB.py
+++ BuildFootballKB.py
@@ -1,115 +1,84 @@
 from urllib import request
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-import os, nltk, math, xml, re, pickle
+import os, re, pickle, io, spacy
 
 from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize
+from collections import Counter
+from string import punctuation
+
+nlp = spacy.load('en_core_web_lg')
 
 
 def grab_urls():
-    url = "https://packerswire.usatoday.com/category/latest-packers-news/"
-    html = request.urlopen(url).read().decode('utf8')
-    soup = BeautifulSoup(html, 'html.parser')
+    targets = ["https://packerswire.usatoday.com/category/latest-packers-news/", "https://www.packers.com/news/", "https://www.espn.com/blog/green-bay-packers", "https://www.acmepackingcompany.com/#stories"]
     list_urls = []
-    counter = 0
-    for link in soup.find_all('a'):
-        if counter == 15:
-            break
-        href = str(link.get('href'))
-        if href.startswith('http'):
-            if href not in list_urls:
-                if re.search('/[\d]{4}/[\d]{2}/[\d]{2}/', href):
-                    list_urls.append(link.get('href'))
-                    counter += 1
-    return list_urls
+
+    for url in targets:
+        parse_url = urlparse(url)
+        html = request.urlopen(url).read().decode('utf8')
+        soup = BeautifulSoup(html, 'html.parser')
+        links = soup.find_all('a')
+        for link in links:
+            href = str(link.get('href'))
+            if href.startswith('http'):
+                if href not in list_urls:
+                    if re.search('/[\d]{4}/[\d]{2}/[\d]{2}/', href):
+                        list_urls.append(link.get('href'))
+                    if re.search('/news/', href):
+                        list_urls.append(link.get('href'))
+            if href.startswith('/news/'):
+                list_urls.append(parse_url.scheme + "://" + parse_url.netloc + link.get('href'))
+            if href.startswith('/blog/green-bay-packers/post/'):
+                list_urls.append(parse_url.scheme + "://" + parse_url.netloc + link.get('href'))
+    return list(set(list_urls))
 
 
 def grab_text(urls):
-    i, files = 1, []
-    for url in urls:
-        html = request.urlopen(url).read().decode('utf8')
-        soup = BeautifulSoup(html, 'html.parser')
-        with open(os.path.join(os.getcwd(), f"url{i}.text"), 'w+') as file:
-            file.write(soup.get_text())
-            file.close()
-            files.append(f"url{i}.text")
-            i += 1
-    return files
+    with io.open(os.path.join(os.getcwd(), f"url_out.text"), 'w+', encoding='utf-8') as f:
+        for url in urls:
+            html = request.urlopen(url).read().decode('utf8')
+            soup = BeautifulSoup(html, 'html.parser')
+            paras = soup.find_all('p')
+            for paragraph in paras:
+                paragraph = paragraph.get_text().replace("\n", "").replace("\t", "")
+                sents = sent_tokenize(paragraph)
+                for sent in sents:
+                    f.write(sent)
+                    f.write('\n')
+    f.close()
 
 
-def clean_text(filenames):
-    i = 1
-    for filename in filenames:
-        file = open(filename, 'r')
-        raw_text = file.read()
-        raw_text = raw_text.replace("\n", "").replace("\t", "")
-        sents = sent_tokenize(raw_text)
-        with open(os.path.join(os.getcwd(), f"url{i}.out"), 'w+') as file:
-            for sent in sents:
-                file.write(sent)
-                file.write('\n')
-            file.close()
-            i += 1
-    return i
+def extract_important(text):
+    """
+    THIS CODE IS INSPIRED BY Ng Wai Foong. HERE'S THE LINK: https://betterprogramming.pub/extractive-text-summarization-using-spacy-in-python-88ab96d1fd97
+    :param text:
+    :return:
+    """
+    important_words = []
+    doc = nlp(text)
+
+    # Generating the most important words based on pos tagging
+    for token in doc:
+        if (token.text not in stopwords.words('english') and token.text not in punctuation) and token.pos_ in ['NOUN', 'PROPN', 'VERB']:
+            important_words.append(token.text)
+
+    # Normalizing the words based on the frequency of the most common word
+    freq_words = Counter(important_words)
+    most_freq_word = freq_words.most_common(1)[0][1]
+    for word in freq_words:
+        freq_words[word] = freq_words[word]/most_freq_word
+
+    return freq_words.most_common(1000)
 
 
-def create_tf_dict(doc):
-    tf_dict = {}
-    tokens = word_tokenize(doc)
-    st = stopwords.words('english')
-    tokens = [w for w in tokens if w.isalpha() and w not in st]
-
-    for t in tokens:
-        if t in tf_dict:
-            tf_dict[t] += 1
-        else:
-            tf_dict[t] = 1
-
-    for t in tf_dict.keys():
-        tf_dict[t] = tf_dict[t]/len(tokens)
-
-    return tf_dict
-
-
-def calc_idf(vocab, vocab_list):
-    idf_dict = {}
-
-    for term in vocab:
-        temp = ['x' for voc in vocab_list if term in voc]
-        idf_dict[term] = math.log((1 + num_docs)/(1 + len(temp)))
-
-    return idf_dict
-
-
-def create_tfidf(tf, idf):
-    tf_idf = {}
-
-    for t in tf.keys():
-        tf_idf[t] = tf[t] * idf[t]
-
-    return tf_idf
-
-
-def extract_important(tf_idfs):
-    important, weights = [], {}
-    for t in tf_idfs:
-        for item, value in sorted(t.items(), key=lambda x: x[1], reverse=True):
-            if value not in weights.values():
-                weights[item] = value
-    s = sorted(weights.items(), key=lambda x: x[1], reverse=True)
-    important.append(s[26:102])
-    return important
-
-
-def build_sentence_bank(files):
+def build_sentence_bank(data_file):
     dbank = []
-    for f in files:
-        with open(f, 'r') as file:
-            raw_text = file.read()
-            raw_text = raw_text.replace("\n", "").replace("\t", "")
-            sents = sent_tokenize(raw_text)
-            for sent in sents:
-                dbank.append(sent)
+    for line in data_file:
+        raw_text = line.replace("\n", "").replace("\t", "")
+        for sent in sent_tokenize(raw_text):
+            dbank.append(sent)
 
     return dbank
 
@@ -126,28 +95,19 @@ def build_kb(terms, files):
 
 if __name__ == "__main__":
     urls = grab_urls()
-    fs = grab_text(urls)
-    num_docs = clean_text(fs)
-    vocab = set()
+    print("------------------------------------- GOT URLS ----------------------------------")
+    grab_text(urls)
+    print("---------------------------------- CLEANED TEXT ---------------------------------")
 
-    docs = []
-    for f in fs:
-        with open(f, 'r') as file:
-            docs.append(file.read().lower().replace('\n', ' '))
+    f = io.open('url_out.text', 'r', encoding='utf-8')
+    important_words = extract_important(f.read())
+    print("---------------------------- GOT IMPORTANT WORDS --------------------------------")
 
-    tfs = []
-    for doc in docs:
-        tfs.append(create_tf_dict(doc))
-
-    for tf in tfs:
-        vocab = vocab.union(set(tf.keys()))
-
-    idf = calc_idf(vocab, tfs)
-    tf_idfs = []
-    for tf in tfs:
-        tf_idfs.append(create_tfidf(tf, idf))
-
-    important_words = extract_important(tf_idfs)
-    print(important_words)
-    # knowledge_base = build_kb(future_words, fs)
-    # pickle.dump(knowledge_base, open('kb.p', 'wb'))
+    for word in ['draft', 'NFL', 'Green Bay Packers', 'offseason', 'preseason', 'free agency', 'snaps', 'touches', 'deals', 'weapons', 'scouts',
+                'talented', 'super bowl', 'regular season', 'college recruits', 'draft day', 'scout tape', 'Lambeau Field', 'Wisconsin',
+                'cheesehead', 'football', 'lombardi trophy', 'championships', 'super bowl rings', 'draft pick']:
+        # These are some reserve words commonly used in the domain of the application's knowledge base.
+        important_words.append(word)
+    knowledge_base = build_kb(list(set(important_words)), f)
+    pickle.dump(knowledge_base, open('kb.p', 'wb'))
+    print("------------------------- FINISHED BUILDING KB ----------------------------------")
